d92dadf859a0077562d556ec432a8f7e650b19cd
Haycode-Dev <ab0064765@setrem.com.br>
2023-03-28 23:28:48 -0300

    first commit

--- src/fetch.js
+++ src/fetch.js
@@ -1,7 +1,19 @@
 // 1. Faça uma requisição GET para a API do Chuck Norris e exiba no console uma piada aleatória
 // https://api.chucknorris.io/jokes/random
+// import fetch from 'node-fetch' 
 async function randomJokes() {
-    
+    try {
+        //await aguarda até que a promise finaliza e fetch busca recursos de froma asincrona na rede
+        const response = await fetch("https://api.chucknorris.io/jokes/random");
+        //data aguarda a promise e converte o conteudo da requisição para .json
+        const data = await response.json();
+        // console.log(data.value);
+        //retorna o valor
+        return data;
+        
+    } catch (error) {
+        console.log(error);
+    }
 }
 randomJokes();
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -9,7 +21,17 @@ randomJokes();
 // 2. Faça uma requisição GET para a API do GitHub e exiba no console os dados retornados
 // https://api.github.com/users/**NOMEDOUSUARIO** 
 async function gitHubUsers() {
-   
+    try {
+        //faz uma requisição http e espera ate que a promise seja concluida
+        const response = await fetch("https://api.github.com/users/nunesfb");
+        //converte para .json
+        const data = await response.json();
+        // console.log(data);
+        return data;
+       
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 gitHubUsers();
--- src/rest.js
+++ src/rest.js
@@ -1,6 +1,11 @@
 // 1. Crie uma função que recebe um número indeterminado de parâmetros e retorne a soma desses parâmetros
 function somar(...numeros) {
-   
+    let total = 0;
+    //cria um for que fica em loop enquant existir numero em numeros e vai adicionando os numeros ao total
+    for (let numero of numeros) {
+        total += numero;
+    }
+    return total;
 }
 
 console.log(somar(1, 2, 3, 4, 5)); // saída esperada: 15
@@ -9,7 +14,13 @@ console.log(somar(10, 20, 30)); // saída esperada: 60
 
 // 2. Crie uma função que recebe um número indeterminado de parâmetros e retorne a média desses parâmetros
 function calcularMedia(...numeros) {
-   
+    let media = 0;
+    //soma dos numero em numeros 
+    for (let numero of numeros) {
+        media += numero;
+    }
+    //media aritimética soma/quantia de numeros
+    return media / numeros.length;
 }
 
 console.log(calcularMedia(1, 2, 3, 4, 5)); // saída esperada: 3
@@ -18,8 +29,10 @@ console.log(calcularMedia(10, 20, 30)); // saída esperada: 20
 
 // 3. Crie uma função que recebe um objeto e um número indeterminado de propriedades e valores, e retorne um novo objeto contendo as propriedades e valores do objeto original e as propriedades e valores recebidos.
 function adicionarPropriedades(objeto, ...novasPropriedades) {
-   
-}
+    //...objeto usa spread para criar um novo objeto que contenha todas as propriedades do objeto "Pessoa"
+    //usando o metodo reduce ele adiciona as novas propriedades com 2 parametros (resultado, propriedade), onde ele junta os dois valores em um só usando spread, e acumula no objeto vazio passado para o spread ("{}")
+    return { ...objeto, ...novasPropriedades.reduce((resultado, propriedade) => ({...resultado, ...propriedade}),{})};
+};
 
 const pessoa = {
     nome: 'Fulano',
