3d84a405d2b39c8bc21e1620868c94dad6302aae
thomaspessoa <thomassoarespessoa.rs@hotmail.com>
2023-03-31 20:36:27 -0300

    start no projeto

--- src/fetch.js
+++ src/fetch.js
@@ -1,15 +1,21 @@
 // 1. Faça uma requisição GET para a API do Chuck Norris e exiba no console uma piada aleatória
 // https://api.chucknorris.io/jokes/random
 async function randomJokes() {
-    
+    //await para esperar a promisse
+    const response = await fetch("https://api.chucknorris.io/jokes/random"); // url da api do chucknorris onde gerar uma piada aleatoria 
+    const data = await response.json(); //precisa utilizar  await por conta da promisse do json
+    return data
 }
-randomJokes();
+randomJokes()
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // 2. Faça uma requisição GET para a API do GitHub e exiba no console os dados retornados
 // https://api.github.com/users/**NOMEDOUSUARIO** 
 async function gitHubUsers() {
-   
+    //await para esperar a promisse
+   const resposta = await fetch("https://api.github.com/users/nunesfb") //url do perfil do git
+   const data = await resposta.json()  //precisa utilizar  await por conta da promisse do json
+   return data
 }
 
 gitHubUsers();
--- src/rest.js
+++ src/rest.js
@@ -1,6 +1,11 @@
 // 1. Crie uma função que recebe um número indeterminado de parâmetros e retorne a soma desses parâmetros
 function somar(...numeros) {
-   
+//recebemos os "restos" e percorremos os valores para realizar a soma
+    let total = 0;
+    for(let numero of numeros) {
+        total += numero
+    }
+    return total;
 }
 
 console.log(somar(1, 2, 3, 4, 5)); // saída esperada: 15
@@ -9,23 +14,35 @@ console.log(somar(10, 20, 30)); // saída esperada: 60
 
 // 2. Crie uma função que recebe um número indeterminado de parâmetros e retorne a média desses parâmetros
 function calcularMedia(...numeros) {
-   
+    // Vai fazer uma comparação se o numero foi zero 
+    if(numeros.length === 0) { 
+        // Não vai receber nenhum valor vai retonra a Nan 
+        return NaN; 
+    }
+    // reduce realiza a soma do primeiro valor com o proximo, assim tendo a soma total
+    const num = numeros.reduce((acumulador,val) => acumulador + val, 0);
+    return num / numeros.length; //dividir o total pelo o tamanho da array 
 }
-
 console.log(calcularMedia(1, 2, 3, 4, 5)); // saída esperada: 3
 console.log(calcularMedia(10, 20, 30)); // saída esperada: 20  
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
-// 3. Crie uma função que recebe um objeto e um número indeterminado de propriedades e valores, e retorne um novo objeto contendo as propriedades e valores do objeto original e as propriedades e valores recebidos.
-function adicionarPropriedades(objeto, ...novasPropriedades) {
-   
+// 3. Crie uma função que recebe um objeto e um número indeterminado de propriedades e valores, e retorne um novo objeto 
+//contendo as propriedades e valores do objeto original e as propriedades e valores recebidos.
+
+function adicionarPropriedades(obj, ...novasPropriedades) {
+    const result = {...obj}; //recebe a primeira propriedade
+    novasPropriedades.forEach((info) => { //percorrer a array 
+        const[test, value] =  Object.entries(info)[0]; //retorna as chaves e valores
+        result[test] = value; //atribui no novo objeto
+    });
+    return result;
 }
 
 const pessoa = {
     nome: 'Fulano',
     idade: 30,
 };
-
 console.log(adicionarPropriedades(pessoa, { cidade: 'São Paulo' }, { profissao: 'Programador' })); // saída esperada: { nome: 'Fulano', idade: 30, cidade: 'São Paulo', profissao: 'Programador' }  
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
