d3716122d00f1f41ad9668bc6dea12280a7863eb
Vitor Serra Cardias <vitorserracardias2@outlook.com>
2023-04-04 19:02:15 -0300

    Não terminei mas tem que entregar

--- arrays.js
+++ arrays.js
@@ -88,34 +88,3 @@ function juntando_arrays(array1, array2){
     // RETORNANDO A JUNÇÃO DOS ARRAYS
     return concatArrays;
 }
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// REST
-
-// 1- FUNÇÃO QUE RECEBE UM NÚMERO INDEFINIDO DE PARAMETROS E RETORNA A SOMA DELES
-
-// USADO O SPREAD(...) PARA INDICAR UM NÚMERO INDEFINIDO DE PARAMETROS
-function soma_rest(...numeros){
-    // VARIAVEL QUE ARMAZENARA A SOMA DE TODO ARRAY
-    let soma = 0;
-    numeros.forEach(function(i){
-        // AQUI É ONDE O SHOW ESTÁ ACONTECENDO
-        soma += i;
-    });
-    // RETORNANDO A SOMA DOS PARAMETROS
-    return soma;
-}
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// 2- FUNÇÃO QUE RECEBE UM NÚMERO INDEFINIDO DE PARAMETROS E RETORNA A MÉDIA DELES
-
-function media_rest(...numeros){
-    // VARIAVEL QUE ARMAZENARA A SOMA DE TODO ARRAY
-    let soma = 0;
-    numeros.forEach(function(i){
-        // AQUI É ONDE É REALIZADO A SOMA DOS NÚMEROS
-        soma += i;
-    });
-    // A MAGICA ACONTECE AQUI, ESTOU RETORNANDO A SOMA DIVIDIDO PELO TAMANHO DO ARRAY, DANDO A MÉDIA
-    return soma / numeros.length;
-}
