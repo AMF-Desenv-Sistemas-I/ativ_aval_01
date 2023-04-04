67232dceaa77bce6b8ee0756b045bcc4c8304b2e
Miquéias Queiroz <84691666+MiqueiasQS@users.noreply.github.com>
2023-04-04 18:49:09 -0300

    Update hofs.js

--- src/hofs.js
+++ src/hofs.js
@@ -1,6 +1,6 @@
 // 1. Crie uma função que receba um objeto como parâmetro e retorne um array contendo as chaves desse objeto
 function obterChaves(objeto) {
-    
+    return Object.keys(objeto); // apenas retorno as chaves do objeto recebido
 }
 
 console.log(obterChaves({ nome: 'Fulano', idade: 30, profissao: 'Programador' })); // saída esperada: ['nome', 'idade', 'profissao']
@@ -14,6 +14,9 @@ function PessoasMaiusculas() {
         { nome: "Maria", idade: 30 },
         { nome: "Pedro", idade: 25 },
     ];
+
+    const nomesUpperCase = pessoasX.map(pessoa => pessoa.nome.toUpperCase()); // utilizo o map para criar um novo array, apenas com os nomes e entro com toUpperCase para deixa-los todos em caixa alta
+    return nomesUpperCase;
 }
 PessoasMaiusculas(); // saída esperada: ["JOÃO", "MARIA", "PEDRO"]
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -25,6 +28,9 @@ function ProdutosPrecoDesconto() {
         { nome: "Calça", preco: 50 },
         { nome: "Jaqueta", preco: 100 },
     ];
+    
+    const valoresComDesconto = produtosX.map(produtos => produtos.preco * 0.9) // novamente utilizo o map para criar um array com os preços, e entro multiplicando por 0.9 (10% de desconto)
+    return valoresComDesconto;
 }
 ProdutosPrecoDesconto(); // saída esperada: [27, 45, 90]
 
@@ -37,6 +43,9 @@ function PessoasMaiorIdade() {
         { nome: "Maria", idade: 15 },
         { nome: "Pedro", idade: 25 },
     ];
+
+    const pessoasMaiores = pessoasY.filter(pessoa => pessoa.idade >= 18); // Utilizo a o médoto filter para remoção das pessoas com menos de 18 anos
+    return pessoasMaiores;
 }
 
 PessoasMaiorIdade(); // saída esperada: [{ nome: "João", idade: 20 }, { nome: "Pedro", idade: 25 }]
@@ -45,6 +54,7 @@ PessoasMaiorIdade(); // saída esperada: [{ nome: "João", idade: 20 }, { nome: 
 // 5. Dada uma lista de strings, crie uma nova lista com as strings que têm mais de 5 caracteres e retorne essa lista
 function StringCincoCaracteres() {
     const palavrasX = ["gato", "cachorro", "leão", "tartaruga"];
+    return palavrasX.filter(palavra => palavra.length > 5); // utilizo p metodo filter para fazer a remoção das palavras que me retornarem 5 ou menos no length
 }
 StringCincoCaracteres(); // saída esperada: ["cachorro", "tartaruga"]
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -56,6 +66,8 @@ function ProdutosMaioresCinquenta() {
         { nome: "Calça", preco: 50 },
         { nome: "Jaqueta", preco: 100 },
     ];
+
+    return produtosY.filter(produto => produto.preco > 50); // filter novamente, dessa vez só comparando o preço do produto como solicitado
 }
 
 ProdutosMaioresCinquenta(); // saída esperada: [{ nome: "Jaqueta", preco: 100 }]
@@ -68,6 +80,12 @@ function CalculaPrecoTotal() {
         { nome: "Calça", preco: 50 },
         { nome: "Jaqueta", preco: 100 },
     ];
+
+    const precoTotal = produtosZ.reduce((total, produto) => { 
+        return total + produto.preco; // utilizo o metodo reduce para somar o preco de todos os produtos e atribuilos na const precoTotal
+    }, 0);
+    
+    return precoTotal; //devolvo precoTotal
 }
 CalculaPrecoTotal(); // saída esperada: 180
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -79,6 +97,10 @@ function ListaObjetoAlunos() {
         { nome: "Maria", idade: 20 },
         { nome: "Pedro", idade: 22 },
     ];
+
+    const nomesAlunos = alunos.map((aluno) => aluno.nome); // apenas utilizo o metodo map para retirar o nome de cada aluno e atribir eo array nomesAlunos
+    console.log(nomesAlunos);
+    return nomesAlunos;
 }
 
 ListaObjetoAlunos();
@@ -91,6 +113,12 @@ function ListaObjetosProdutos() {
         { nome: "Calça", preco: 50 },
         { nome: "Jaqueta", preco: 100 },
     ];
+    const precoTotal = produtosA.reduce((total, produto) => {
+        return total + produto.preco; /// utilizo o metodo reduce para somar o preco de todos os produtos e atribuilos na const precoTotal (mesma coisa da 7)
+    }, 0);
+    
+    console.log(precoTotal); 
+    return precoTotal; // tive que retornar se não não passava nos testes
 }
 
 ListaObjetosProdutos();
@@ -103,6 +131,8 @@ function ListaProdutoMaiorQueDez() {
         { nome: "Calça", preco: 50 },
         { nome: "Jaqueta", preco: 100 },
     ];
+
+    return produtosB.every(produto => produto.preco > 10); // utilizo metodo every que faz a validação (produto.preco > 10), retornando true se todos se enquadrarem na condição, se não retorna false
 }
 ListaProdutoMaiorQueDez();
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -110,6 +140,8 @@ ListaProdutoMaiorQueDez();
 // 11. Dada uma lista de nomes, verifique se pelo menos um deles começa com a letra "A”
 function NomeComLetraA() {
     const nomes = ["Pedro", "Ana", "Carla", "Márcio"];
+
+    return nomes.some(nome => nome.startsWith('A')); // utilizo o metodo some, verificando com o metodo startsWith se pelo menos um dos nomes do array começa com a letra 'A', caso contrario retorna false
 }
 
 NomeComLetraA();
@@ -122,6 +154,8 @@ function ArrayObjetosJoao(){
         { nome: "João", idade: 25 },
         { nome: "Maria", idade: 40 },
     ];
+
+    return pessoasA.find((objeto) => objeto.nome === "João"); // utilizo o metodo every para verificar se algum nome do objeto se enquadra na minha condição (nome === "João")
 }
 
 ArrayObjetosJoao();
@@ -134,6 +168,10 @@ function ArrayObjetosMaiorIdade(){
         { nome: "João", idade: 25 },
         { nome: "Maria", idade: 40 },
     ];
+
+    return pessoasB.reduce((pessoaMaisVelha, pessoa) => { // retorno o resultado que o metodo reduce me devolver
+        return pessoa.idade > pessoaMaisVelha.idade ? pessoa : pessoaMaisVelha; // com o loop do reduce, verifico um por um se é maior que o outro, o resultado é semelhante ao que é conhecido como "Método Bolha", mas nesse caso reduce facilita bastante
+    });
 }
 
 ArrayObjetosMaiorIdade();
