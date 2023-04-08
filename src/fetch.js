77893fb86c2ea462f8cd4d716209b1131e81d990
TimoteoMalves <timoteo.alves14@gmail.com>
2023-04-08 12:20:08 -0300

    done

--- src/fetch.js
+++ src/fetch.js
@@ -1,7 +1,7 @@
 // 1. Faça uma requisição GET para a API do Chuck Norris e exiba no console uma piada aleatória
 // https://api.chucknorris.io/jokes/random
 async function randomJokes() {
-    
+    return fetch("https://api.chucknorris.io/jokes/random").then(response => response.json()).then(data => console.log(data.value));
 }
 randomJokes();
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -9,7 +9,7 @@ randomJokes();
 // 2. Faça uma requisição GET para a API do GitHub e exiba no console os dados retornados
 // https://api.github.com/users/**NOMEDOUSUARIO** 
 async function gitHubUsers() {
-   
+    return fetch("https://api.github.com/users/TimoteoMalves ").then(response => response.json()).then(data => console.log(data));
 }
 
 gitHubUsers();
--- src/rest.js
+++ src/rest.js
@@ -1,6 +1,8 @@
 // 1. Crie uma função que recebe um número indeterminado de parâmetros e retorne a soma desses parâmetros
 function somar(...numeros) {
-   
+    
+    let total = numeros.reduce((acumulator, number)=> acumulator + number);
+    return total;
 }
 
 console.log(somar(1, 2, 3, 4, 5)); // saída esperada: 15
@@ -9,16 +11,28 @@ console.log(somar(10, 20, 30)); // saída esperada: 60
 
 // 2. Crie uma função que recebe um número indeterminado de parâmetros e retorne a média desses parâmetros
 function calcularMedia(...numeros) {
-   
+    let total = numeros.reduce((acumulator, number)=> acumulator + number);
+    let avg = total/numeros.length;
+    return avg;
 }
 
 console.log(calcularMedia(1, 2, 3, 4, 5)); // saída esperada: 3
 console.log(calcularMedia(10, 20, 30)); // saída esperada: 20  
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
-// 3. Crie uma função que recebe um objeto e um número indeterminado de propriedades e valores, e retorne um novo objeto contendo as propriedades e valores do objeto original e as propriedades e valores recebidos.
+// 3.  Crie uma função que recebe um objeto e um número indeterminado de propriedades e valores, e retorne um novo objeto contendo as propriedades e valores do objeto original e as propriedades e valores recebidos
 function adicionarPropriedades(objeto, ...novasPropriedades) {
    
+    let newObj = objeto;
+    
+    novasPropriedades.forEach(prop => {
+        novasPropriedades.entries(prop).forEach(
+            ([key,value])=> {
+                newObj[key] = value;
+            });
+        });
+
+    return newObj;
 }
 
 const pessoa = {
@@ -26,7 +40,7 @@ const pessoa = {
     idade: 30,
 };
 
-console.log(adicionarPropriedades(pessoa, { cidade: 'São Paulo' }, { profissao: 'Programador' })); // saída esperada: { nome: 'Fulano', idade: 30, cidade: 'São Paulo', profissao: 'Programador' }  
+console.log(adicionarPropriedades(pessoa, { cidade: 'São Paulo' }, { profissao: 'Programador' })) // saída esperada: { nome: 'Fulano', idade: 30, cidade: 'São Paulo', profissao: 'Programador' }  
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 export { somar, calcularMedia, adicionarPropriedades };
